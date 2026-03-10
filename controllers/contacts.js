9e8a15f0aae549f0ad623a5529d4717753d619b2
schnaider30-programmer <sjeanlouis@byupathway.edu>
2026-03-10 00:20:08 -0400

    change endpoint users to contact

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -9,12 +9,9 @@ const getAll = async (req, res) => {
 }
 const getSingle = async (req, res) => {
     const userID = new objectId(req.params.id)
-    console.log(userID)
     const result = await mongodb.getDb().collection("contacts").findOne({ _id: userID })
-    console.log(result)
     res.setHeader("Content-Type", "application/json")
     res.status(200).json(result)
-    console.log(JSON.stringify(result))
 
 }
 
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongoDb = require('./data/database')
 
 const port = process.env.PORT || 3000;
 
-app.use("/", (req, res) => {
+app.get("/", (req, res) => {
     res.send("Hello WOrld")
 })
 
